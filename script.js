ca5522b3bd01a735194675471e381e22b5ed36db
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-12-12 13:04:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,12 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 
